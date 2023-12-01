7733cad7c60a89a22821f78a346c3808fb61b144
fahryanputra <fahryanputra@gmail.com>
2023-12-01 14:45:28 +0700

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = 0;
+  result = (temp - 32) * (5 / 9);
+  result = Math.round(result * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = 0;
+  result = (temp * (9 / 5) + 32);
+  result = Math.round(result * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
