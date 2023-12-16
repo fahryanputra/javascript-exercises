1fa5cd51de54e4a55c7cbccd7d9f77960aa1ea9f
fahryanputra <fahryanputra@gmail.com>
2023-12-16 11:24:34 +0700

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // make the string case insensitive
+    str = str.toUpperCase();
+    // remove punctuation and white space from the string
+    let regex = /[^a-z0-9]/gi;
+    str = str.replace(regex, "");
+    str = str.replace(" ", "");
+    // reverse the string
+    let reversed = str.split("");
+    reversed = reversed.reverse();
+    reversed = reversed.join("");
+    // check palindrome
+    if(str === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
