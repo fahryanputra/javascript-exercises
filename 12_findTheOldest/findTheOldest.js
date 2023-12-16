78993740c63628fb986017fd1517392bc1e1a452
fahryanputra <fahryanputra@gmail.com>
2023-12-16 12:18:10 +0700

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    // replace missing yearOfDeath with current year
+    const currentYear = new Date().getFullYear();
+    arr.forEach(item => {
+        if(!item.yearOfDeath) {
+            item.yearOfDeath = currentYear;
+        }
+    })
+    const oldest = arr.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    return oldest[0]
 };
 
 // Do not edit below this line
