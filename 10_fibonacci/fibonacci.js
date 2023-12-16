92b07be68325684536286938d7b6d5f5f2d9bec2
fahryanputra <fahryanputra@gmail.com>
2023-12-16 11:40:37 +0700

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) {
+        return "OOPS";
+    }
+    let arr = [];
+    for(let i = 0; i <= +num; i++) {
+        if(i === 0) {
+            arr.push(0);
+        } else if(i === 1) {
+            arr.push(1);
+        } else {
+            arr.push(arr[i - 1] + arr [i - 2]);
+        }
+    }
 
+    return arr[num];
 };
 
 // Do not edit below this line
